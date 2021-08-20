9c744494c663d6fae4aa53a2d4a854529c11c0fb
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-08-20 18:46:27 -0300

    js change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const apiKey = "09d548a33db0542ad09ffa093324e81a";
 const fetchData = (position) => {
   const { latitude, longitude } = position.coords; //desestructuring para obtener data de esos dos datos especificos //
   const myPetition = fetch(
-    `http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${apiKey}`
   );
   myPetition
     .then((response) => response.json())
