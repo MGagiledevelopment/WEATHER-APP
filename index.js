5f9717b5ee458c62a8b7122c0285e983006190e9
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-08-20 15:47:03 -0300

    function setting

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const apiKey = '09d548a33db0542ad09ffa093324e81a'
 
 const fetchData = position =>{
+
+
     const { latitude, longitude } = position.coords //desestructuring para obtener data de esos dos datos especificos //
  const myPetition = fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
    myPetition.then(response => response.json())
-   .then(data => console.log(data))
+   .then(data => setWeatherData(data))
    
-console.log(position)
+
+}
+
+const setWeatherData = data =>{
+    console.log(data)
+
+    const weatherInformation = {
+        location: data.name,
+        condition: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: data.main.temp,
+        date:'proximamente'
+    }
+
+    Object.keys(weatherInformation).forEach((key) => {
+        document.getElementById(key).textContent = weatherInformation[key]
+    })
+
 }
 
 const onLoad = () => {
 navigator.geolocation.getCurrentPosition(fetchData)
 
 }
+
 
